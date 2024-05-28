dc86afa210b10d3ddfd727c623fee7be09989786
sshussain <suheel_hussain@yahoo.com>
2024-05-28 12:18:27 -0400

    Regenerated package list

--- src/pdqs/user_admin.py
+++ src/pdqs/user_admin.py
@@ -2,3 +2,11 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 user_admin = Blueprint('user_admin', __name__, template_folder='templates')
+
+
+@user_admin.route('/login')
+def show(page):
+    try:
+        return render_template(f'pages/{page}.html')
+    except TemplateNotFound:
+        abort(404)
